7cae4297c316ef1c7420c492fc04185ee444732c
Regis Tesson <registesson@gmail.com>
2021-03-28 14:24:35 +0200

    15. Toggling Components

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,9 +8,10 @@ import { loadDetails } from '../actions/detailAction'
 import { Link } from 'react-router-dom'
 
 const Game = ({ name, released, image, id }) => {
-  //Load Details
+  //Load Details Handler
   const dispatch = useDispatch()
   const loadDetailHandler = () => {
+    document.body.style.overflow = 'hidden'
     dispatch(loadDetails(id))
   }
 
@@ -31,6 +32,7 @@ const StyledGame = styled(motion.div)`
   text-align: center;
   border-radius: 1rem;
   cursor: pointer;
+  overflow: hidden;
   img {
     width: 100%;
     height: 40vh;
