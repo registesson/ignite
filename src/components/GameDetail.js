e6bfd97d239b935b577cbea45d3301dd7e2877cb
Regis Tesson <registesson@gmail.com>
2021-03-30 13:05:19 +0200

    17. Component Transitions

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -7,7 +7,7 @@ import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router'
 import { smallImage } from '../util'
 
-const GameDetail = () => {
+const GameDetail = ({ pathId }) => {
   const history = useHistory()
   //Exit Detail
   const exitDetailHandler = (e) => {
@@ -24,10 +24,10 @@ const GameDetail = () => {
     <>
       {!isLoading && (
         <CardShadow className="shadow" onClick={exitDetailHandler}>
-          <Detail>
+          <Detail layoutId={pathId}>
             <Stats>
               <div className="rating">
-                <h3>{game.name}</h3>
+                <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                 <p>Rating: {game.rating}</p>
               </div>
               <Info>
@@ -40,7 +40,8 @@ const GameDetail = () => {
               </Info>
             </Stats>
             <Media>
-              <img
+              <motion.img
+                layoutId={`image ${pathId}`}
                 src={smallImage(game.background_image, 1280)}
                 alt={game.background_image}
               />
